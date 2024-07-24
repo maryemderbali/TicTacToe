77b0bd47b6c6c1cc3107b4948065c3d87657728c
maryemderbali <derbalimaryem@gmail.com>
2024-07-24 17:19:21 +0200

    tictactoee

--- tictactoe.js
+++ tictactoe.js
@@ -39,19 +39,19 @@ function setTile() {
         return;
     }
 
-    let coords = this.id.split("-");    //ex) "1-2" -> ["1", "2'"]
+    let coords = this.id.split("-");   
     let r = parseInt(coords[0]);
     let c = parseInt(coords[1]);
 
     if (board[r][c] != ' ') { 
-        //already taken spot
+      
         return;
     }
     
-    board[r][c] = currPlayer; //mark the board
-    this.innerText = currPlayer; //mark the board on html
+    board[r][c] = currPlayer; 
+    this.innerText = currPlayer; 
 
-    //change players
+ 
     if (currPlayer == playerO) {
         currPlayer = playerX;
     }
@@ -59,17 +59,16 @@ function setTile() {
         currPlayer = playerO;
     }
 
-    //check winner
+
     checkWinner();
 }
 
 
 function checkWinner() {
-    //horizontally, check 3 rows
+  
     for (let r = 0; r < 3; r++) {
         if (board[r][0] == board[r][1] && board[r][1] == board[r][2] && board[r][0] != ' ') {
-            //if we found the winning row
-            //apply the winner style to that row
+          
             for (let i = 0; i < 3; i++) {
                 let tile = document.getElementById(r.toString() + "-" + i.toString());
                 tile.classList.add("winner");
@@ -79,11 +78,10 @@ function checkWinner() {
         }
     }
 
-    //vertically, check 3 columns
+   
     for (let c = 0; c < 3; c++) {
         if (board[0][c] == board[1][c] && board[1][c] ==  board[2][c] && board[0][c] != ' ') {
-            //if we found the winning col
-            //apply the winner style to that col
+           
             for (let i = 0; i < 3; i++) {
                 let tile = document.getElementById(i.toString() + "-" + c.toString());                
                 tile.classList.add("winner");
@@ -93,7 +91,6 @@ function checkWinner() {
         }
     }
 
-    //diagonally
     if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ') {
         for (let i = 0; i < 3; i++) {
             let tile = document.getElementById(i.toString() + "-" + i.toString());                
@@ -103,17 +100,13 @@ function checkWinner() {
         return;
     }
 
-    //anti-diagonally
     if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ') {
-        //0-2
         let tile = document.getElementById("0-2");                
         tile.classList.add("winner");
 
-        //1-1
         tile = document.getElementById("1-1");                
         tile.classList.add("winner");
 
-        //2-0
         tile = document.getElementById("2-0");                
         tile.classList.add("winner");
         gameOver = true;
